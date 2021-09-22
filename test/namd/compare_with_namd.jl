68f05780ef726ed803195417c24a0372a41a2ac5
Leandro Martinez <leandro@iqm.unicamp.br>
2021-09-22 09:26:23 -0300

    added some additional tests

--- test/namd/compare_with_namd.jl
+++ test/namd/compare_with_namd.jl
@@ -17,9 +17,9 @@ function getcoor(file)
     return reinterpret(reshape,SVector{3,Float64},Chemfiles.positions(frame))
 end
 
-function test_newcl(file,unit_cell,correct)
+function test_newcl(file,unit_cell,correct,lcell)
     coordinates = getcoor(file)
-    box = Box(unit_cell, 10.)
+    box = Box(unit_cell, 10., lcell=lcell)
     cl = CellList(coordinates,box)
     u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
     return u ≈ correct
@@ -29,15 +29,17 @@ end
 
   dir="./namd"
 
+  lcell=1
+
   # Some orthorhombic cells
 
   unit_cell = [ 50.,   50.,  50. ]
   correct = 32230.01699504111
-  @test test_newcl("$dir/o1.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o1.dcd", unit_cell, correct, lcell)
   
   unit_cell = [ 80.,  70.,  50. ]
   correct = 1093.7225407797744
-  @test test_newcl("$dir/o2.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o2.dcd", unit_cell, correct, lcell)
 
   # Orthorhombic but rotated
   
@@ -45,23 +47,23 @@ end
                 50.     50.      0. 
                  0.     50.     50. ]
   correct = 1724.3195067566828
-  @test test_newcl("$dir/o3.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o3.dcd", unit_cell, correct, lcell)
   
   unit_cell= transpose([ 70.7107   0.0      0.0
                          35.3553  61.2372   0.0
                          35.3553  20.4124  57.735 ])
   correct = 1754.0802503953591
-  @test test_newcl("$dir/o4.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o4.dcd", unit_cell, correct, lcell)
   
   unit_cell= transpose([ 70.7107   0.0      0.0
                          35.3553  61.2372   0.0
                          35.3553  20.4124  57.735 ])
   correct = 1765.1389457850137
-  @test test_newcl("$dir/o5.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o5.dcd", unit_cell, correct, lcell)
   
   unit_cell = [ 80.,  80., 80. ]
   correct = -158.04751357760088
-  @test test_newcl("$dir/o6.dcd", unit_cell, correct)
+  @test test_newcl("$dir/o6.dcd", unit_cell, correct, lcell)
 
   # Some triclinic cells
   
@@ -69,13 +71,13 @@ end
                 30.     80.      0. 
                 0.     40.     80. ]
   correct = -116.53213607052128
-  @test test_newcl("$dir/t1.dcd", unit_cell, correct)
+  @test test_newcl("$dir/t1.dcd", unit_cell, correct, lcell)
   
   unit_cell = [ 50.      0.      0.
                 50.     50.      0. 
                 0.     50.     50. ]
   correct = 32096.48839031735
-  @test test_newcl("$dir/t2.dcd", unit_cell, correct)
+  @test test_newcl("$dir/t2.dcd", unit_cell, correct, lcell)
 
   #
   # Check cell list updating routine
@@ -86,7 +88,7 @@ end
                  0.     50.      0. 
                  0.      0.     50. ]
   correct = 32230.01699504111
-  box = Box(unit_cell, 10.)
+  box = Box(unit_cell, 10., lcell=lcell)
   cl = CellList(coordinates,box)
   u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
   @test u ≈ correct
@@ -94,7 +96,7 @@ end
   coordinates = getcoor("$dir/o2.dcd")
   unit_cell = [ 80.,  70., 50. ]
   correct = 1093.7225407797744
-  box = Box(unit_cell, 10.)
+  box = Box(unit_cell, 10., lcell=lcell)
   cl = UpdateCellList!(coordinates,box,cl)
   u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
   @test u ≈ correct
@@ -106,7 +108,93 @@ end
                 30.     80.      0. 
                 0.     40.     80. ]
   correct = -116.53213607052128
-  box = Box(unit_cell, 10.)
+  box = Box(unit_cell, 10., lcell=lcell)
+  cl = UpdateCellList!(coordinates,box,cl,aux)
+  u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
+  @test u ≈ correct
+
+  # Same thing with a different lcell
+
+  lcell=3
+
+  # Some orthorhombic cells
+
+  unit_cell = [ 50.,   50.,  50. ]
+  correct = 32230.01699504111
+  @test test_newcl("$dir/o1.dcd", unit_cell, correct, lcell)
+  
+  unit_cell = [ 80.,  70.,  50. ]
+  correct = 1093.7225407797744
+  @test test_newcl("$dir/o2.dcd", unit_cell, correct, lcell)
+
+  # Orthorhombic but rotated
+  
+  unit_cell = [ 50.      0.     50.
+                50.     50.      0. 
+                 0.     50.     50. ]
+  correct = 1724.3195067566828
+  @test test_newcl("$dir/o3.dcd", unit_cell, correct, lcell)
+  
+  unit_cell= transpose([ 70.7107   0.0      0.0
+                         35.3553  61.2372   0.0
+                         35.3553  20.4124  57.735 ])
+  correct = 1754.0802503953591
+  @test test_newcl("$dir/o4.dcd", unit_cell, correct, lcell)
+  
+  unit_cell= transpose([ 70.7107   0.0      0.0
+                         35.3553  61.2372   0.0
+                         35.3553  20.4124  57.735 ])
+  correct = 1765.1389457850137
+  @test test_newcl("$dir/o5.dcd", unit_cell, correct, lcell)
+  
+  unit_cell = [ 80.,  80., 80. ]
+  correct = -158.04751357760088
+  @test test_newcl("$dir/o6.dcd", unit_cell, correct, lcell)
+
+  # Some triclinic cells
+  
+  unit_cell = [ 80.      0.     30.
+                30.     80.      0. 
+                0.     40.     80. ]
+  correct = -116.53213607052128
+  @test test_newcl("$dir/t1.dcd", unit_cell, correct, lcell)
+  
+  unit_cell = [ 50.      0.      0.
+                50.     50.      0. 
+                0.     50.     50. ]
+  correct = 32096.48839031735
+  @test test_newcl("$dir/t2.dcd", unit_cell, correct, lcell)
+
+  #
+  # Check cell list updating routine
+  #
+
+  coordinates = getcoor("$dir/o1.dcd")
+  unit_cell = [ 50.      0.      0.
+                 0.     50.      0. 
+                 0.      0.     50. ]
+  correct = 32230.01699504111
+  box = Box(unit_cell, 10., lcell=lcell)
+  cl = CellList(coordinates,box)
+  u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
+  @test u ≈ correct
+
+  coordinates = getcoor("$dir/o2.dcd")
+  unit_cell = [ 80.,  70., 50. ]
+  correct = 1093.7225407797744
+  box = Box(unit_cell, 10., lcell=lcell)
+  cl = UpdateCellList!(coordinates,box,cl)
+  u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
+  @test u ≈ correct
+
+  # Test preallocated AuxThreaded struct
+  aux = CellListMap.AuxThreaded(cl)
+  coordinates = getcoor("$dir/t1.dcd")
+  unit_cell = [ 80.      0.     30.
+                30.     80.      0. 
+                0.     40.     80. ]
+  correct = -116.53213607052128
+  box = Box(unit_cell, 10., lcell=lcell)
   cl = UpdateCellList!(coordinates,box,cl,aux)
   u = map_pairwise!((x,y,i,j,d2,u) -> lj_NE(d2,u),0.0,box,cl)
   @test u ≈ correct
