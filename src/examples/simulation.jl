4078c0f6799637025906768bab6a01e92ff70ba2
Leandro Martinez <leandro@iqm.unicamp.br>
2022-08-13 23:07:47 -0300

    update simulation

--- src/examples/simulation.jl
+++ src/examples/simulation.jl
@@ -1,39 +1,39 @@
 using StaticArrays
 using CellListMap.PeriodicSystems
 import CellListMap.wrap_relative_to
-# Function that updates the forces (potential of the form (d^2-radius)^2
-function update_forces!(x, y, i, j, d2, forces, radius)
-    dx = x - y
-    if d2 < radius^2
-        dfdx = 4 * dx * (d2 - radius)
-        forces[i] -= dfdx
-        forces[j] += dfdx
-    end
+# Function that updates the forces, for potential of the form:
+# if d < cutoff k*(d^2-cutoff^2)^2 else 0.0 with k = 10^6
+function update_forces!(x, y, i, j, d2, forces, cutoff)
+    r = y - x
+    dudr = 10^6 * 4 * r * (d2 - cutoff^2)
+    forces[i] += dudr
+    forces[j] -= dudr
     return forces
 end
-function simulate(; N::Int=1000, nsteps::Int=100, isave=1)
+function simulate(; N::Int=200, nsteps::Int=100, isave=1)
     Vec2D = SVector{2,Float64}
     positions = rand(Vec2D, N)
     unitcell = [1.0, 1.0]
+    cutoff = 0.1
     system = PeriodicSystem(
         positions=positions,
-        cutoff=0.1,
+        cutoff=cutoff,
         unitcell=unitcell,
         output=similar(positions),
         output_name=:forces,
+        parallel=false,
     )
     velocities = [-1.0 .+ 2.0 * randn(Vec2D) for _ in 1:N]
     dt = 1e-3
-    radius = 0.5
     trajectory = typeof(positions)[]
     for step in 1:nsteps
         # compute forces at this step
         map_pairwise!(
-            (x,y,i,j,d2,forces) -> update_forces!(x,y,i,j,d2,forces,radius),
+            (x,y,i,j,d2,forces) -> update_forces!(x,y,i,j,d2,forces,cutoff),
             system
         )
         # Update positions and velocities
-        for i in eachindex(system.forces)
+        for i in eachindex(system.positions, system.forces)
             f = system.forces[i]
             x = system.positions[i]
             v = velocities[i]
