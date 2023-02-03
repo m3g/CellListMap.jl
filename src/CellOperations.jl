2b9ad6020b2cf007d9246c7a65181db0598fc13a
Leandro Martinez <lmartine@unicamp.br>
2023-02-03 09:39:03 -0300

    trying to get particle_cell to work

--- src/CellOperations.jl
+++ src/CellOperations.jl
@@ -425,8 +425,21 @@ and the `cell_size` vector. The computing box is always Orthorhombic, and the fi
 computing box with positive coordinates has indexes `Box.lcell + 1`.
 
 """
-@inline particle_cell(x::SVector{N}, box::Box) where N =
-    CartesianIndex(ntuple(i -> floor(Int,x[i]/box.cell_size[i]) + box.lcell + 1, N))
+#@inline particle_cell(x::SVector{N}, box::Box) where N =
+#    CartesianIndex(ntuple(i -> floor(Int,x[i]/box.cell_size[i]) + box.lcell + 1, N))
+
+@inline function particle_cell(x::SVector{N}, box::Box) where {N}
+    CartesianIndex(
+        ntuple(N) do i
+            xmin = zero(eltype(x)) - box.cell_size[i]
+            xmax = box.unit_cell_max[i] + box.cell_size[i]
+            xi = fix_upper_boundary(x[i], xmin, xmax)
+            xi = (xi - xmin) / box.cell_size[i]
+            index = floor(Int, xi) + 1
+            return index
+        end
+    )
+end
 
 """
 
