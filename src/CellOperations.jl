4fd6c26a291bf021d97ac1d837d8e3b122d0cf3d
Leandro Martinez <lmartine@unicamp.br>
2023-02-13 09:29:52 -0300

    improved error message

--- src/CellOperations.jl
+++ src/CellOperations.jl
@@ -285,7 +285,7 @@ Returns the index of the cell, in the 1D representation, from its cartesian coor
 # Compute the maximum and minimum coordinates of the vectors composing the particle sets
 #
 function _minmax(x::AbstractVector{<:AbstractVector})
-    length(x) <= 0 && throw(ArgumentError("Cannot set unitcell box from coordinates without particles."))
+    length(x) <= 0 && throw(ArgumentError("Cannot set unitcell box from empty coordinates vector."))
     N = size(x[begin], 1)
     T = eltype(x[begin])
     xmin = fill(typemax(T), MVector{N,T})
