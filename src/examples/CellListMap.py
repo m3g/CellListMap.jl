6260fe4a4f7e3a6d8478e2ca1d94ef2563c396ed
Leandro Martinez <lmartine@unicamp.br>
2023-08-21 10:43:23 -0300

    make python indexes 0-based and run GC manually

--- src/examples/CellListMap.py
+++ src/examples/CellListMap.py
@@ -18,6 +18,9 @@ end
 # Neighboring pairs of a single set of particles
 def neighborlist(x, cutoff, unitcell=None) :
     x_t = x.transpose()
+    # Garbage collection must be turned off because it does not 
+    # interact nicely with python objects, which may be cleaned up
+    # causing memory corruption
     jl.GC.enable(False)
     nb_list = jl.neighborlist(x_t, cutoff, unitcell=unitcell)
     jl.GC.enable(True)
@@ -25,11 +28,20 @@ def neighborlist(x, cutoff, unitcell=None) :
     j_inds = np.full((len(nb_list),), 0, dtype=np.int64)
     d = np.full((len(nb_list),), 0.0, dtype=np.float64)
     jl.copy_to_numpy_arrays(nb_list, i_inds, j_inds, d)
+    # convert to 0-based indexing
+    i_inds -= 1
+    j_inds -= 1
+    # Run julia garbage collection
+    jl.seval("nb_list = nothing")
+    jl.GC.gc()
     return i_inds, j_inds, d
 # Neighboring pairs of two sets of particles
 def neighborlist_cross(x, y, cutoff, unitcell=None) :
     x_t = x.transpose()
     y_t = y.transpose()
+    # Garbage collection must be turned off because it does not 
+    # interact nicely with python objects, which may be cleaned up
+    # causing memory corruption
     jl.GC.enable(False)
     nb_list = jl.neighborlist(x_t, y_t, cutoff, unitcell=unitcell)
     jl.GC.enable(True)
@@ -37,4 +49,10 @@ def neighborlist_cross(x, y, cutoff, unitcell=None) :
     j_inds = np.full((len(nb_list),), 0, dtype=np.int64)
     d = np.full((len(nb_list),), 0.0, dtype=np.float64)
     jl.copy_to_numpy_arrays(nb_list, i_inds, j_inds, d)
+    # convert to 0-based indexing
+    i_inds -= 1
+    j_inds -= 1
+    # Run julia garbage collection
+    jl.seval("nb_list = nothing")
+    jl.GC.gc()
     return i_inds, j_inds, d
