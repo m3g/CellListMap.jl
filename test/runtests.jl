51bd698ba1fd6fe7c20f03fbf998ca6782e5ef88
Leandro Martinez <lmartine@unicamp.br>
2025-01-31 17:24:08 -0300

    move AllocTest module to package code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,5 @@
 using TestItemRunner
 
-@testmodule AllocTest begin
-    # This module defines the Allocs struct and the comparison operators
-    # to conditionally compare the number of allocations based on the
-    # BUILD_IS_PRODUCTION_BUILD environment variable.
-    export Allocs
-    @kwdef struct Allocs
-        prodbuild::Bool = haskey(ENV, "BUILD_IS_PRODUCTION_BUILD") && ENV["BUILD_IS_PRODUCTION_BUILD"] == "true"
-        allocs::Int
-    end
-    Allocs(allocs::Int) = Allocs(; allocs)
-    import Base: ==, >, <
-    ==(a::Int, b::Allocs) = b.prodbuild ? a == b.allocs : true
-    <(a::Int, b::Allocs) = b.prodbuild ? a < b.allocs : true
-    ==(a::Allocs, b::Int) = a.prodbuild ? a.allocs == b : true
-    <(a::Allocs, b::Int) = a.prodbuild ? a.allocs < b : true
-end
-
 @testitem "Aqua.test_all" begin
     import Aqua
     Aqua.test_all(CellListMap)
