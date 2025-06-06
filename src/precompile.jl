566814505dfb39d00b2d21308fddc6c6cac2edf8
Leandro Martinez <lmartine@unicamp.br>
2025-06-06 11:03:52 -0300

    reduce precompilation work

--- src/precompile.jl
+++ src/precompile.jl
@@ -4,16 +4,18 @@ if Base.VERSION >= v"1.9"
         # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
         # precompile file and potentially make loading faster.
         cutoff = 0.05
-        x3d = rand(3, 100)
-        y3d = rand(3, 100)
+        x3d = rand(3, 10)
+        y3d = rand(3, 10)
         u3d = [ 1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0 ]
-        x2d = rand(2, 100)
-        y2d = rand(2, 100)
+        x2d = rand(2, 10)
+        y2d = rand(2, 10)
         u2d = [ 1.0 0.0; 0.0 1.0 ]
-        x, box = CellListMap.xatomic(5000)
-        f(x,y,i,j,d2,out) = out += d2
+        f(_,_,_,_,d2,out) = out += d2
         PrecompileTools.@compile_workload begin
-            cl = CellList(x,box)
+            box = Box(u3d, cutoff)
+            cl = CellList(x3d, box)
+            box = Box(u2d, cutoff)
+            cl = CellList(x2d, box)
             map_pairwise!(f, 0.0, box, cl)
             neighborlist(x3d, y3d, cutoff, unitcell=u3d)
             neighborlist(x3d, cutoff, unitcell=u3d)
