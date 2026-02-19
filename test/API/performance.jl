1815b9b4481ccac7e5a18f07b1db55346d9595b3
Leandro Martinez <lmartine@unicamp.br>
2026-02-19 14:43:58 -0300

    do not propagate input parameters

--- test/API/performance.jl
+++ test/API/performance.jl
@@ -1,5 +1,6 @@
 @testitem "performance" begin
-    let cmd = `$(Base.julia_cmd()) --startup-file=no --check-bounds=auto ../modules/performance.jl`
+    let cmd = `$(Base.julia_cmd()[1]) --startup-file=no --check-bounds=auto ../modules/performance.jl`
+        println(cmd)
         success(pipeline(cmd; stdout=stdout, stderr=stderr)) || error("performance check, cmd : $cmd")
     end
 end
