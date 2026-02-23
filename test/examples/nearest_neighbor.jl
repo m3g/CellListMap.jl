fb4b8a4dce1028e609a9003cd828600516294297
lmiq <lmartine@unicamp.br>
2026-02-22 21:34:54 -0300

    remove file with redundant code

--- test/examples/nearest_neighbor.jl
+++ test/examples/nearest_neighbor.jl
@@ -49,3 +49,49 @@ function nearest_neighbor(; N1 = 1_500, N2 = 1_500_000, parallel = true, x = not
     return (mind.i, mind.j, mind.d)
 
 end
+
+#
+# In this test we compute the minimum distance between two independent sets of particles,
+# without periodic conditions. We need to define a special reducing function to the
+# parallel computation.
+#
+function nearest_neighbor_nopbc(; N1 = 1_500, N2 = 1_500_000, parallel = true, x = nothing, y = nothing)
+
+    # Particle positions
+    Random.seed!(321)
+    if x === nothing
+        x = [ rand(SVector{3, Float64}) for i in 1:N1 ]
+    else
+        N1 = length(x)
+    end
+    if y === nothing
+        y = [ rand(SVector{3, Float64}) for i in 1:N2 ]
+    else
+        N2 = length(y)
+    end
+
+    # Obtain one upper bound for dmin by computing one distance for each element
+    # of the smallest vector
+    cutoff = +Inf
+    for v in x
+        iy = rand(1:N2)
+        cutoff = min(CellListMap.norm(v - y[iy]), cutoff)
+    end
+
+    sys = ParticleSystem(
+        xpositions=x,
+        ypositions=y,
+        cutoff=cutoff,
+        output=Mind(0,0,+Inf),
+        parallel=parallel,
+    )
+
+    # Function that keeps the minimum distance
+    f(pair, mind) = pair.d < mind.d ? Mind(pair.i, pair.j, pair.d) : mind
+
+    # Run pairwise computation
+    mind = pairwise!(f, sys)
+
+    # Take the square root of the minimum distance to return
+    return (mind.i, mind.j, mind.d)
+end
