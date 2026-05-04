f546e84f3cfa19a4bc8a603538b51265e44db42e
Leandro Martinez <lmartine@unicamp.br>
2026-05-04 11:29:36 -0300

    adjust test

--- test/internals/NonPeriodicCells.jl
+++ test/internals/NonPeriodicCells.jl
@@ -15,7 +15,7 @@
     sys2 = ParticleSystem(xpositions=x, ypositions=y, cutoff=0.1, output=0.0, nbatches=(4,4))
     @test parse_show(sys2._aux; repl = Dict("CellListMap." => "")) ≈ """ 
         CellListMap.AuxNonPeriodicPair{3, Float64}
-        Auxiliary arrays for nbatches = 4
+        Auxiliary arrays for nbatches = (4, 4)
     """
 
 end
